ec81131396df48fcb75064a0f155933b26477b7e
Chen Guo <chen.guo.0625@gmail.com>
2022-02-08 13:02:51 -0800

    [DSP-69] init TS server

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+
+const app = express();
+
+
+const DefaultPort = 9125;
+
+function getPort(): number {
+  const port = parseInt(process.env.PORT);
+
+  if (isNaN(port)) {
+    return DefaultPort;
+  } else {
+    return port;
+  }
+}
+
+const port = getPort();
+app.listen(port, () => {
+  console.log('Node server listening on port', port);
+});
