762159b22ec0461045a39931581ace336c27749c
Chen Guo <chen.guo.0625@gmail.com>
2022-02-09 12:36:07 -0800

    [DSP-69] move codegen to scripts/

---
+++ scripts/gen-py-pb.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROTO_DIR=$1
+
+if [ -z "$PROTO_DIR" ]; then
+    PROTO_DIR=$DIR/../proto
+fi
+
+(
+    cd $DIR
+    python -m grpc_tools.protoc \
+           -I$PROTO_DIR \
+           --python_out=$DIR/../py-sidecar/app \
+           --grpc_python_out=$DIR/../py-sidecar/app \
+           $PROTO_DIR/app.proto
+)
