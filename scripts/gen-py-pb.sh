8ff4901a0422ca54fa190773b15900f96deebdb4
Chen Guo <chen.guo.0625@gmail.com>
2022-02-09 18:32:14 -0800

    [DSP-69] some clean-up

--- scripts/gen-py-pb.sh
+++ scripts/gen-py-pb.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROTO_DIR=$1
-
-if [ -z "$PROTO_DIR" ]; then
-    PROTO_DIR=$DIR/../proto
-fi
+APP_DIR=$DIR/../py-sidecar
 
 cd $DIR
 python -m grpc_tools.protoc \
-       -I$PROTO_DIR \
-       --python_out=$DIR/../py-sidecar/app \
-       --grpc_python_out=$DIR/../py-sidecar/app \
-       $PROTO_DIR/add-things.proto
-
+       -I$APP_DIR/proto \
+       --python_out=$APP_DIR/app \
+       --grpc_python_out=$APP_DIR/app \
+       $APP_DIR/proto/add-things.proto
